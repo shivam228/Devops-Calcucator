a86a2ec23675fe1eed173ffe9a1941ec16e72553
shivam228 <shivamjain229@gmail.com>
2021-02-13 18:55:11 +0530

    Added my project

---
+++ src/test/java/TestSuites.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        Calcuator_test.class,
+     
+})
+public class TestSuites {
+    // the class remains empty
+    // used only as a holder for the above annotations
+}
